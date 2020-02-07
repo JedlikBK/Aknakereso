7369def1c62e067f4ee03188c783184a1d7a7f5f
Bella.Kevin.László <Bella.Kevin.Lszl@C102-17>
2020-02-07 10:03:28 +0100

    Kirajzoló Advanced

--- Aknakereso/Aknakereso/Program.cs
+++ Aknakereso/Aknakereso/Program.cs
@@ -8,53 +8,90 @@ namespace Aknakereso
 {
     class Program
     {
-        char[,] pálya = new char [10,10];
+        
 
-        static void Main(string[] args)
+        static void Feltöltés(char[,] pálya)
+        {  
+            for (int i = 0; i < pálya.GetLength(0); i++)
+			{
+                for (int j = 0; j < pálya.GetLength(1); j++)
+			    {
+                    pálya[i,j] = '_';
+			    }
+			}
+        }
+
+        static void Main(string[] args)  // Függvény: megadja, hogy az adott hely körül mennyi bomba van!!!!
         {
-            char[,] pálya = new char [10, 10];
+            char[,] pálya=new char[10,10];
+            
+            Console.Write("Bombák száma: ");
+            int bombaszám = int.Parse(Console.ReadLine());
             Feltöltés(pálya);
-            Kirajzoló(pálya);
+            Bombasorsoló(pálya, bombaszám);
+            bool legyenbomba= false;
+            Kirajzoló(pálya, legyenbomba);  
+            int indexX;
+            int indexY;
+            do
+	        {
+                Lépés(pálya, out indexX, out indexY);    // Bekér 1 sor és egy 1 oszlop indexet és kirak egy 'X'-et ha nincs ott bomba, van akkor irja ki hogy felrobbantál
+	        } while (pálya[indexX,indexY]!='B');
+                             
             Console.ReadKey();
         }
-        static void Feltöltés(char[,] pálya)
+        static void Bombasorsoló (char[,] pálya, int bombaszám)
         {
+            Random gép= new Random();
             int sor;
             int oszlop;
-            for (sor = 0; sor < pálya.GetLength(0); sor++)
-            {
-                for (oszlop = 0; oszlop < pálya.GetLength(1); oszlop++)
-                {
-                    pálya[sor, oszlop] = '_';
-                }
-            }
-            Random gép = new Random();
-            
-            for (int i = 0; i < 10; i++)
-            {
+            for (int i = 0; i < bombaszám; i++)
+			{
                 do
-                {
-                    sor = gép.Next(0, 10);
-                    oszlop = gép.Next(0, 10);
-                } while (pálya[sor,oszlop] != '_');
-                pálya[sor, oszlop] = 'B';
-              
-            }
+	            {
+                    sor = gép.Next(10);
+                    oszlop = gép.Next(10);
+	            } while (pálya[sor,oszlop] == 'B');
+                pálya[sor,oszlop] = 'B';
+			}
         }
-        private static void Kirajzoló(char[,] pálya)
+        static void Kirajzoló (char[,] pálya, bool legyenbomba)
         {
-            
-            for (int sor = 0; sor < pálya.GetLength(0); sor++)
-            {
-                for (int oszlop = 0; oszlop < pálya.GetLength(1); oszlop++)
-                {
-                    
-                    Console.Write($"{pálya[sor, oszlop]}|");
-                }
+            for (int i = 0; i < pálya.GetLength(0); i++)
+			{
+                for (int j = 0; j < pálya.GetLength(1); j++)
+			    {
+                    if (legyenbomba)
+	                {
+                        Console.Write(pálya[i,j]);
+	                }
+                    else if (pálya[i,j]!='X')
+	                {
+                        Console.Write('_');
+	                }
+                    Console.Write('|');
+			    }
+	
+                
                 Console.WriteLine();
-            }
-            
+			}
         }
-
+        static void Lépés (char[,] pálya, out int indexX, out int indexY)
+        {            
+            Console.Write("x = ");
+            indexX = int.Parse(Console.ReadLine())-1;
+            Console.Write("y = ");
+            indexY = int.Parse(Console.ReadLine())-1;
+            if (pálya[indexX,indexY] == 'B')
+	        {
+                Console.WriteLine("you ded");
+	        }else
+	        {
+                pálya[indexX,indexY] = 'X';
+                Kirajzoló(pálya, false);
+	        }
+        }
+        
+        
     }
 }
